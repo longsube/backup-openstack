ac78e90dee52975e5a65522d36f0cf57d8495627
thanhha123 <thienhabka@gmail.com>
2014-12-02 15:55:09 +0700

    Sua duong dan

--- up-backup/upbackup.sh
+++ up-backup/upbackup.sh
@@ -3,16 +3,17 @@
 HOST='172.16.69.161'
 USER='admin'
 PASSWD='a'
+DuongDan='/root/mysql_folder/'
 # Doi ten duong dan khi thuc hien
-FILE=`ls -1t /root/mysql_folder/ | head -1`
+FILE=`ls -1t $DuongDan | head -1`
 
 /usr/bin/expect - << EOF
    spawn sftp $USER@$HOST
         expect "password:"
         send "$PASSWD\r"
         expect "sftp>"
-        send "put /root/mysql_folder/$FILE\r"
-        send "exit\r"
+        send "put $DuongDan$FILE\r"
         expect "sftp>"
+        send "exit\r"
         interact
 EOF
