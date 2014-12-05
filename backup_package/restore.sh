1279b71db0f72e6aaaba486c1eedccbfdfcbfdf9
Truong Thanh Ha <thienhabka@gmail.com>
2014-12-05 16:54:35 +0700

    Update restore.sh

--- backup_package/restore.sh
+++ backup_package/restore.sh
@@ -4,7 +4,7 @@ directory1="/var/cache/apt/archives/"
 echo -e “please put password:  \c ”
 read word
 
-find . -maxdepth 1 -name "*.tar" -exec  unzip -P $word *.zip \;
+find . -maxdepth 1 -name "*.zip" -exec  unzip -P $word *.zip \;
 if [ -d $directory1 ]
 then
 rsync -rv /var/ /var/cache/apt/archives/
