d1bce7631026532515cb459b2a82f98412f326a1
Truong Thanh Ha <thienhabka@gmail.com>
2014-12-05 15:33:27 +0700

    Update backuppackage.sh

--- backuppackage.sh
+++ backuppackage.sh
@@ -2,8 +2,26 @@
 nameSRV=`hostname`
 ngay=`eval date +%d%m%Y-%H%M%S`
 cd /var/cache/apt/archives/
-tar -cvf /root/"$nameSRV""_""$ngay""_package".tar *deb
-echo " Finish!!!"
+tenfile="$nameSRV""_""$ngay""_package".tar
+tar -cvf /root/backuppackage/$tenfile *deb
+
+
+
+HOST='172.16.69.161'
+USER='admin'
+PASSWD='a'
+cd /root/backuppackage/
+
+/usr/bin/expect - << EOF
+   spawn sftp $USER@$HOST
+        expect "password:"
+        send "$PASSWD\r"
+        expect "sftp>"
+        send "put $tenfile \r"
+        expect "sftp>"
+        send "exit\r"
+        interact
+EOF
 #
 # Khai bao duong dan vao day
 # find $backup_dir -ctime 7 -type f -delete
